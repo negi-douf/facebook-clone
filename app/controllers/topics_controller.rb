1486049731ec6377323acb3e18179f7f1c14c1a2
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-09-26 22:05:26 +0900

    add create, strong parameter, errors, flash

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,10 @@
 class TopicsController < ApplicationController
+  before_action :set_errors, only: [:index, :edit]
+
+  # エラー出力用のグローバル変数
+  # エラーメッセージを格納する
+  $errors = []
+
   def index
     # 投稿用
     @topic = Topic.new
@@ -7,6 +13,15 @@ class TopicsController < ApplicationController
   end
 
   def create
+    @topic = Topic.new(topics_params)
+    if @topic.save
+      flash[:success] = "Topicを投稿しました！"
+      redirect_to root_path
+    else
+      flash[:danger] = "ツイートの投稿に失敗しました"
+      $errors = @tweet.errors
+      redirect_to root_path
+    end
   end
 
   def show
@@ -19,5 +34,19 @@ class TopicsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  # strong parameter
+  def topics_params
+    params.require(:topic).permit(:content)
+  end
+
+  def set_errors
+    unless $errors.empty?
+      @errors = $errors
+      $errors = []
+    end
   end
 end
