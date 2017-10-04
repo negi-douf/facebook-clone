57c3f2f4d06b9fda7fc3258b157153f7a8622aaa
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-10-04 20:32:50 +0900

    add comments#destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update, :destroy]
+
   def create
     @comment = current_user.comments.build(comments_params)
     @topic = @comment.topic
@@ -22,11 +24,25 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @topic = @comment.topic
+
+    respond_to do |format|
+      if @comment.destroy
+        format.html { redirect_to topic_path(@topic) }
+        format.js { render :index }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   private
 
   def comments_params
     params.require(:comment).permit(:topic_id, :content)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 end
