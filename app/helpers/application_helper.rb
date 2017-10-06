3e6b72875ff480c7c1b737ab77eded92d8adfc38
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-10-06 12:39:07 +0900

    fixed show avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,13 +9,15 @@ module ApplicationHelper
   end
 
   def profile_img(user)
-    return image_tag(user.avatar, alt: user.name) if user.avatar?
-
-    unless user.provider.blank?
+    return image_tag(user.avatar, alt: user.name) unless user.avatar.blank?
+    return image_tag(user.image_url, alt: user.name) if user.image_url != nil
+    if ! user.image_url.blank?
       img_url = user.image_url
+    elsif ! user.avatar.blank?
+      img_url = user.avatar
     else
       img_url = 'no_image.png'
     end
-    image_tag(img_url, alt: user.name)
+    image_tag(img_url)
   end
 end
